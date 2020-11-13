5c9230b4089ba884076de30610da5e3bc1b4c855
Zed Chance <zchance@csus.edu>
2020-11-13 15:29:29 -0800

    Add GiftCardTest

---
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -0,0 +1,51 @@
+package csc131.junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class GiftCardTest
+{
+	@Test
+	void getIssuingStore()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore());
+	}
+	
+	@Test
+	void getBalance()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("getBalance()", balance, card.getBalance(), 0.001);
+	}
+	
+	@Test
+	void deduct()
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		
+		assertEquals("deduct()", "Remaining Balance:  50.00", card.deduct(50));
+	}
+}
